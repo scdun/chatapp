54d53c120dcbc8a252051e868974c5b1770b2eb6
scdun <102838439+scdun@users.noreply.github.com>
2023-01-20 16:20:30 -0600

    Added files

---
+++ src/pages/Register.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Weevil Chat</span>
+                <span className="title">Register</span>
+                <form>
+                    <input type="text" placeholder='display name' />
+                    <input type="email" placeholder='email' />
+                    <input type="password" placeholder='password' />
+                    <input style={{display:"none"}} type="file" id="file"/>
+                    <label htmlFor="file">
+                        <img src="" alt="" />
+                    </label>
+                    <button>Sign up</button>
+                </form>
+                <p>Already have an account? Login</p>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
